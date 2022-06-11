de9f17a569dd11ad944c256a098c43ced438ab69
Katsutami7moto <nclsf87@gmail.com>
2022-06-11 15:58:34 +0300

    Generate index page from template; display winery's actual age

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+import datetime
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+now_year = datetime.datetime.now().year
+est_year = 1920
+age = now_year - est_year
+
+age_last_number = int(str(age)[2:])
+if age_last_number == 1:
+    years = 'год'
+elif age_last_number in (2, 3, 4):
+    years = 'года'
+else:
+    years = 'лет'
+
+rendered_page = template.render(
+    winery_age=age,
+    years_word_form=years,
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
+
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
